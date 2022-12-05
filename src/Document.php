921ef90efc5706d26d97e84abeb55f4a384c81f8
Laloux <loic.laloux@allopneus.com>
2022-12-05 17:00:33 +0100

    feat(redisearch): multiple modifications
    TECH-990

--- src/Document.php
+++ src/Document.php
@@ -2,148 +2,161 @@
 
 namespace AllopneusRediSearch;
 
-class Document {
+class Document
+{
 
-  /**
-   * @var mixed|string
-   */
-  protected $id;
+    /**
+     * @var mixed|string
+     */
+    protected $id;
 
-  /**
-   * @var float
-   */
-  protected $score = NULL;
+    /**
+     * @var float
+     */
+    protected $score = NULL;
 
-  /**
-   * @var bool
-   */
-  protected $replace = FALSE;
+    /**
+     * @var bool
+     */
+    protected $replace = FALSE;
 
-  /**
-   * @var array
-   */
-  protected $fields = array();
+    /**
+     * @var array
+     */
+    protected $fields = array();
 
-  /**
-   * @var string
-   */
-  protected $language = NULL;
+    /**
+     * @var string
+     */
+    protected $language = NULL;
 
 
-  public function __construct( string $id = null ) {
-    $this->id = $id ?? uniqid(true);
-  }
-
-  /**
-   * Get set fields as a one dimensional array
-   *
-   * @return array
-   */
-  public function getDefinition() {
-    $properties = array();
-
-    foreach ( $this->getFields() as $name => $value) {
-      if ( isset( $value ) ) {
-        $properties[] = $name;
-        $properties[] = $value;
-      }
+    public function __construct(string $id = null)
+    {
+        $this->id = $id ?? uniqid(true);
     }
 
-    return $properties;
-  }
+    /**
+     * Get set fields as a one dimensional array
+     *
+     * @return array
+     */
+    public function getDefinition()
+    {
+        $properties = array();
 
-  /**
-   * Setting associative array of field => values
-   *
-   * @param array $fields
-   *
-   * @return object Document
-   */
-  public function setFields( array $fields = array() ) {
-    $this->fields = $fields;
-    return $this;
-  }
+        foreach ($this->getFields() as $name => $value) {
+            if (isset($value)) {
+                $properties[] = $name;
+                $properties[] = $value;
+            }
+        }
 
-  /**
-   * Get fields
-   *
-   * @return array
-   */
-  public function getFields() {
-    return $this->fields;
-  }
-
-  /**
-   * Get document ID
-   * @return string
-   */
-  public function getId()  {
-    return $this->id;
-  }
-
-  /**
-   * Set document ID
-   * @param string $id
-   *
-   * @return object Document
-   */
-  public function setId( string $id ) {
-    $this->id = $id;
-    return $this;
-  }
-
-  /**
-   * Get document score
-   *
-   * @return float|null
-   */
-  public function getScore() {
-    return $this->score;
-  }
-
-  /**
-   * Set score for the document.
-   *
-   * @param float $score
-   *
-   * @return object Document
-   */
-  public function setScore( float $score ) {
-    if ($score < 0.0 || $score > 1.0) {
-      $score = 1.0;
+        return $properties;
     }
 
-    $this->score = $score;
-    return $this;
-  }
+    /**
+     * Setting associative array of field => values
+     *
+     * @param array $fields
+     *
+     * @return object Document
+     */
+    public function setFields(array $fields = array())
+    {
+        $this->fields = $fields;
+        return $this;
+    }
 
-  public function isReplace() {
-    return $this->replace;
-  }
+    /**
+     * Get fields
+     *
+     * @return array
+     */
+    public function getFields()
+    {
+        return $this->fields;
+    }
 
-  public function setReplace( bool $replace ) {
-    $this->replace = $replace;
-    return $this;
-  }
+    /**
+     * Get document ID
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-  /**
-   * Return document language if set specifically
-   *
-   * @return string|null
-   */
-  public function getLanguage() {
-    return $this->language;
-  }
+    /**
+     * Set document ID
+     * @param string $id
+     *
+     * @return object Document
+     */
+    public function setId(string $id)
+    {
+        $this->id = $id;
+        return $this;
+    }
 
-  /**
-   * Return document specific language if set
-   * @param string|null $language
-   *
-   * @return object Document
-   */
-  public function setLanguage( string $language ) {
-    $this->language = $language;
-    return $this;
-  }
+    /**
+     * Get document score
+     *
+     * @return float|null
+     */
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /**
+     * Set score for the document.
+     *
+     * @param float $score
+     *
+     * @return object Document
+     */
+    public function setScore(float $score)
+    {
+        if ($score < 0.0 || $score > 1.0) {
+            $score = 1.0;
+        }
+
+        $this->score = $score;
+        return $this;
+    }
+
+    public function isReplace()
+    {
+        return $this->replace;
+    }
+
+    public function setReplace(bool $replace)
+    {
+        $this->replace = $replace;
+        return $this;
+    }
+
+    /**
+     * Return document language if set specifically
+     *
+     * @return string|null
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * Return document specific language if set
+     * @param string|null $language
+     *
+     * @return object Document
+     */
+    public function setLanguage(string $language)
+    {
+        $this->language = $language;
+        return $this;
+    }
 
 }
--- src/RedisRaw/PredisAdapter.php
+++ src/RedisRaw/PredisAdapter.php
@@ -10,47 +10,94 @@ use Predis\Client;
  *
  * This class wraps the NRK client: https://github.com/nrk/predis
  */
-class PredisAdapter extends AbstractRedisRawClient {
-  /**
-   * @var Client
-   */
-  public $redis;
-  
-  public function connect($hostname = '127.0.0.1', $port = 6379, $db = 0, $password = null, $scheme = 'tcp'): RedisRawClientInterface {
-    $clientArgs = array(
-      'database'  => $db,
-      'password'  => $password
-    );
-    if ( $scheme === 'tcp' ) {
-      $clientArgs = array_merge(
-        $clientArgs,
-        array(
-        'scheme'    => 'tcp',
-        'port'      => $port,
-        'host'      => $hostname
-        )
-      );
-    } else if ( $scheme === 'unix' ) {
-      $clientArgs = array_merge(
-        $clientArgs,
-        array(
-        'scheme'    => 'unix',
-        'path'      => $hostname
-        )
-      );
+class PredisAdapter implements RedisRawClientInterface
+{
+    /**
+     * @var Client
+     */
+    public $redis;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function connect(string $hostname = '127.0.0.1', int $port = 6379, int $db = 0, ?string $password = null, string $scheme = 'tcp'): RedisRawClientInterface
+    {
+        $clientArgs = array(
+            'database' => $db,
+            'password' => $password
+        );
+        if ($scheme === 'tcp') {
+            $clientArgs = array_merge(
+                $clientArgs,
+                array(
+                    'scheme' => 'tcp',
+                    'port' => $port,
+                    'host' => $hostname
+                )
+            );
+        } else if ($scheme === 'unix') {
+            $clientArgs = array_merge(
+                $clientArgs,
+                array(
+                    'scheme' => 'unix',
+                    'path' => $hostname
+                )
+            );
+        }
+        $this->redis = new Client($clientArgs);
+        $this->redis->connect();
+
+        return $this;
     }
-    $this->redis = new Client( $clientArgs );
-    $this->redis->connect();
-    return $this;
-  }
-  
-  public function multi(bool $usePipeline = NULL) {
-    return $this->redis->pipeline();
-  }
-  
-  public function rawCommand(string $command, array $arguments) {
-    $preparedArguments = $this->prepareRawCommandArguments($command, $arguments);
-    $rawResult = $this->redis->executeRaw($preparedArguments);
-    return $this->normalizeRawCommandResult($rawResult);
-  }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function multi()
+    {
+        return $this->redis->pipeline();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rawCommand(string $command, array $arguments)
+    {
+        $preparedArguments = $this->prepareRawCommandArguments($command, $arguments);
+        $rawResult = $this->redis->executeRaw($preparedArguments);
+
+        return $this->normalizeRawCommandResult($rawResult);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function flushAll(): void
+    {
+        $this->redis->flushAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function prepareRawCommandArguments(string $command, array $arguments): array
+    {
+        foreach ($arguments as $index => $argument) {
+            if (!is_scalar($arguments[$index])) {
+                $arguments[$index] = (string)$argument;
+            }
+        }
+
+        array_unshift($arguments, $command);
+
+        return $arguments;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function normalizeRawCommandResult($rawResult)
+    {
+        return $rawResult === 'OK' ? true : $rawResult;
+    }
 }
